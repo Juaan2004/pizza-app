55331ca84d35830e4da5de4e58fd5691c8962ff9
Juaan2004 <jeduardo211204@gmail.com>
2024-10-25 23:47:40 +0000

    edicion de mavbar lista

---
+++ src/app/service/pizza.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Pizza } from '../model/pizza';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PizzaService {
+  private apiURL = 'http://localhost:3000/pizzas'
+
+  constructor(private http: HttpClient) { }
+
+  getPizzas(): Observable<Pizza[]>{
+    return this.http.get<Pizza[]>(this.apiURL)
+  }
+
+  getPizza(id: number): Observable<Pizza>{
+    return this.http.get<Pizza>(`${this.apiURL}/{id}`)
+  }
+
+  addPiza(pizza: Pizza): Observable<Pizza>{
+    return this.http.post<Pizza>(this.apiURL, pizza)
+  }
+
+  updatePizza(id: number, pizza: Pizza): Observable<Pizza>{
+    return this.http.put<Pizza>(`${this.apiURL}/{id}`, pizza)
+  }
+
+  deletePizza(id: number): Observable<void>{
+    return this.http.delete<void>(`${this.apiURL}/{id}`)
+  }
+}
